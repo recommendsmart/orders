c5abf46638d344679eaccbf3b4574717b1e26ea4
recommendsmart <37797329+recommendsmart@users.noreply.github.com>
2019-12-23 17:12:50 +0530

    add order line item

---
+++ src/Plugin/Field/FieldFormatter/OrderLineItemFormatter.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\orders\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'order_field_line_item' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "order_line_item_formatter",
+ *   module = "orders",
+ *   label = @Translation("Order Line Item formatter"),
+ *   field_types = {
+ *     "order_line_item"
+ *   }
+ * )
+ */
+class OrderLineItemFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    foreach ($items as $delta => $item) {
+      $rows[$delta] = array(
+        '#type' => 'html_tag',
+        '#tag' => 'p',
+        '#value' => $this->t('The color code in this field is @code', array('@code' => $item->value)),
+      );
+    }
+    foreach ($items as $delta => $item) {
+      // Calculated fields.
+      $base_price = $item->quantity * $item->amount;
+      $total_price = $base_price * (1 + $item->gst / 100);
+
+      // Build the row.
+      $rows[$delta] = [
+        'item' => $item->item,
+        'quantity' => $item->quantity,
+        'amount' => $item->amount,
+        'gst' => $item->gst . '%',
+		    'discount' => $item->discount . '%',
+        'base_price' => $base_price,
+        'total_price' => $total_price,
+      ];
+    }
+    $header = [
+      'Description',
+      'Quantity',
+      'Unit Price',
+      'GST',
+	    'Discount',
+      'Price before Tax',
+      'Price with Tax',
+    ];
+    $elements = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+    ];
+
+    return $elements;
+  }
+
+}
---
+++ src/Plugin/Field/FieldWidget/OrderLineItemWidget.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\orders\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Witget for order_line_item elements.
+ *
+ * @FieldWidget(
+ *   id = "order_line_item_widget",
+ *   label = @Translation("Order Line item widget"),
+ *   field_types = {
+ *     "order_line_item"
+ *   }
+ * )
+ */
+class OrderLineItemWidget extends WidgetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $elements['item'] = [
+      '#type' => 'textfield',
+      '#title' => t('Description'),
+      '#default_value' => $items->get($delta)->get('item')->getValue(),
+      '#size' => 30,
+      '#maxlength' => 128,
+    ];
+
+    $quantity = $items->get($delta)->get('quantity')->getValue();
+    $quantity = (!is_null($quantity) ? $quantity : 1);
+    $elements['quantity'] = [
+      '#type' => 'number',
+      '#title' => t('Quantity'),
+      '#default_value' => $quantity,
+      '#size' => 2,
+      '#scale' => 2,
+      '#step' => 0.01,
+      '#maxlength' => 4,
+      '#attributes' => ['class' => ['quantity']],
+    ];
+
+    $amount = $items->get($delta)->get('amount')->getValue();
+    $amount = (!is_null($amount) ? $amount : 0);
+    $elements['amount'] = [
+      '#type' => 'number',
+      '#title' => t('Unit Price'),
+      '#default_value' => $amount,
+      '#size' => 4,
+      '#scale' => 2,
+      '#maxlength' => 6,
+      '#step' => 0.01,
+      '#attributes' => ['class' => ['amount']],
+    ];
+
+    $gst = $items->get($delta)->get('gst')->getValue();
+    $gst = (!is_null($gst) ? $gst : 0);
+
+    $config = \Drupal::service('config.factory')->get('orders.settings');
+    $gstOptions = $config->get('gst_types');
+    $options = [];
+    foreach ($gstOptions as $gstOption) {
+      $value = $gstOption['value'];
+      $gstType = $gstOption['value_label'];
+      $options[$value] = $gstType;
+    };
+
+    $elements['gst'] = [
+      '#type' => 'select',
+      '#title' => t('GST'),
+      '#default_value' => $gst,
+      '#options' => $options,
+      '#attributes' => ['class' => ['gst']],
+    ];
+
+    $discount = $items->get($delta)->get('discount')->getValue();
+    $discount = (!is_null($discount) ? $discount : 0);
+    $elements['discount'] = [
+      '#type' => 'number',
+      '#title' => t('Discount'),
+      '#default_value' => $discount,
+      '#size' => 2,
+      '#scale' => 2,
+      '#step' => 0.01,
+      '#maxlength' => 4,
+      '#attributes' => ['class' => ['quantity']],
+    ];
+
+	$elements['base_price'] = [
+      '#type' => 'number',
+      '#title' => t('Price before Tax'),
+      '#default_value' => $quantity * $amount,
+      '#step' => 0.01,
+      '#size' => 10,
+      '#scale' => 2,
+      '#maxlength' => 10,
+      '#step' => 0.01,
+      '#attributes' => ['class' => ['base_price']],
+    ];
+
+    $elements['total_price'] = [
+      '#type' => 'number',
+      '#title' => t('Price with Tax/Discount'),
+      '#default_value' => ($quantity * $amount * (1 - $discount * (0.01))) * (1 + $gst * (0.01)),
+      '#size' => 10,
+      '#scale' => 2,
+      '#step' => 0.01,
+      '#maxlength' => 12,
+      '#attributes' => ['class' => ['total_price']],
+    ];
+    return $elements;
+  }
+
+}
